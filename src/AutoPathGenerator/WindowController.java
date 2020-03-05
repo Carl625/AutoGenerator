22b0821fb9e6b9b9cee194bd6427a160c9751261
Carlos-Montemayor <carlos.a.montemayor.01@gmail.com>
2020-03-04 20:28:23 -0500

    still working on design, got the basics of the set-up worked out

--- src/AutoPathGenerator/WindowController.java
+++ src/AutoPathGenerator/WindowController.java
@@ -2,6 +2,7 @@ package AutoPathGenerator;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
@@ -14,11 +15,26 @@ public class WindowController {
     public Canvas fieldDisplay;
     public GraphicsContext graphics;
     public Image field;
+    public ChoiceBox defStone;
+    public ChoiceBox alliance;
 
     public void initialize() {
 
+        initTabs();
+    }
+
+    private void initTabs() {
+
+        initPathSpec();
+        initAutoInit();
+        initAutoMisc();
+        initAutoGen();
+    }
+
+    private void initPathSpec() {
+
         try {
-            FileInputStream f = new FileInputStream("../AutoPathGenerator/src/Resources/Images/topviewfield.png");
+            FileInputStream f = new FileInputStream("../AutoGenerator/src/Resources/Images/topviewfield.png");
             field = new Image(f);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -26,6 +42,30 @@ public class WindowController {
 
         graphics = fieldDisplay.getGraphicsContext2D();
         resetField();
+    }
+
+    private void initAutoInit() {
+
+        defStone.getItems().clear();
+        defStone.getItems().add("Left");
+        defStone.getItems().add("Middle");
+        defStone.getItems().add("Right");
+        defStone.getSelectionModel().select(1);
+
+        alliance.getItems().clear();
+        alliance.getItems().add("Red");
+        alliance.getItems().add("Blue");
+        alliance.getSelectionModel().select(0);
+    }
+
+    private void initAutoMisc() {
+
+
+    }
+
+    private void initAutoGen() {
+
+
     }
 
     public void fieldClicked(MouseEvent mouseEvent) {
